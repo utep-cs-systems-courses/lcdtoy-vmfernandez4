82da3fec48717e7a10238704d0e7701087fa4a37
Victor <vmfernandez4@miners.utep.edu>
2020-05-03 23:20:00 -0600

    test2

--- project/buzzer.h
+++ project/buzzer.h
@@ -1,6 +1,11 @@
 #ifndef buzzer_included
 #define buzzer_included
 
+void buzzer_init();
+void buzzer_set_period(short cycles);
+
+extern char jurrasic_enable, siren_enable, playing_note; 
+
 // Notes
 #define a 300
 #define b 450
